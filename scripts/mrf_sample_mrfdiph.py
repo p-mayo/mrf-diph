038b45ef9f6761beca99f1be3de363c2288fe9dd
Mohammad Golbabaee <41708196+mgolbabaee@users.noreply.github.com>
2025-09-17 14:19:38 +0100

    Update mrf_sample_mrfdiph.py

--- scripts/mrf_sample_mrfdiph.py
+++ scripts/mrf_sample_mrfdiph.py
@@ -1,6 +1,6 @@
 """
 Generate a large batch of image samples from a model and save them as a large
-numpy array. This can be used to produce samples for FID evaluation.
+numpy array. This can be used to produce samples for FID evaluation..
 """
 import os
 import gc
